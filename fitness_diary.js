48ab30b139f0763c13ba2f739a37a24741365d13
Marcus T <marcustan8998@gmail.com>
2025-05-08 11:04:41 +1000

    Added alert if nothing is selected and user tries to go to next page

--- fitness_diary.js
+++ fitness_diary.js
@@ -3,9 +3,27 @@ const currentDate = new Date();
 document.getElementById("today").innerHTML = "Current date: " + currentDate.toLocaleDateString();
 
 //Making the text mandatory if "other" is checked
-const button = document.getElementById("otherbutton");
+const otherbutton = document.getElementById("otherbutton");
 const other = document.getElementById("other")
 
-button.addEventListener("change", () => {
+otherbutton.addEventListener("change", () => {
     other.required = button.checked;
 });
+
+//Alerting if nothing has been selected
+function activeStatus(){
+    const inputs = document.querySelectorAll('input[type="checkbox"]');
+    let active = false;
+    inputs.forEach((input) => {
+        if (input.checked){
+            active = true;
+        };
+    });
+    if(!active){
+        alert("Please select at least one option to continue.")
+    }
+}
+
+const submitButton = document.getElementById("continue");
+submitButton.addEventListener("click", activeStatus)
+
