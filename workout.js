71a7a69cf32d263187685896e011350bfa095fe7
Marcus T <marcustan8998@gmail.com>
2025-05-10 17:12:20 +1000

    Added function where selecting muscle group from dropdown populates exercise fields

--- workout.js
+++ workout.js
@@ -7,3 +7,48 @@ for (i = 0; i < params.length; i++){
         document.getElementById(params[i] + 'routine').style.display = 'initial';
     }
 }
+
+//Changing exercise fields based on selected musle group
+const muscleGroupDropdown = document.getElementById('musclegroup');
+const exercises = [];
+const exerciseColumns = document.querySelectorAll('tr')[2].querySelectorAll('td').length
+for (i = 1; i < exerciseColumns; i++){
+    exercises.push(document.getElementById('exercise' + i.toString()))
+}
+
+const chestWorkout = ['Dumbbell bench', 'Incline dumbbell bench', 'Machine chest flies', 'Dips', 'Cable pulldowns', 'Overhead tricep extensions'];
+const shoulderWorkout = ['Dumbbell shoulder press', 'Machine shoulder press', 'Dips', 'Shoulder Flies', 'Cable pulldowns', 'Overhead tricep extensions'];
+const backWorkout = ['Barbell rows', 'Lat pulldown', 'Seated rows', 'Machine rows', 'Machine curls', 'Incline curls'];
+const legWorkout = ['Squats', 'Leg extensions', 'Hamstring curls', 'Calf raises', 'Incline curls', 'Cable pulldowns'];
+
+function populateExercises(){
+    let currentSelection = muscleGroupDropdown.value;
+    switch(currentSelection){
+        case 'chest':
+            for (i=0; i < exercises.length; i++){
+                exercises[i].innerHTML = chestWorkout[i]
+            }
+            break;
+        case 'shoulders':
+            for (i=0; i < exercises.length; i++){
+                exercises[i].innerHTML = shoulderWorkout[i]
+            }
+            break;
+        case 'back':
+            for (i=0; i < exercises.length; i++){
+                exercises[i].innerHTML = backWorkout[i]
+            }
+            break;
+        case 'legs':
+            for (i=0; i < exercises.length; i++){
+                exercises[i].innerHTML = legWorkout[i]
+            }
+            break;
+        default:
+            for (i=0; i < exercises.length; i++){
+                exercises[i].innerHTML = ''
+            }
+    }
+}
+
+muscleGroupDropdown.addEventListener('change', populateExercises);
