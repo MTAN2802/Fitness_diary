e31c3f01a05478c9b22939da23cc6c802e59ffb2
Marcus T <marcustan8998@gmail.com>
2025-08-05 18:48:06 +1000

    Fixed export python code to export each csv file that exists

--- export_to_postgresql.py
+++ export_to_postgresql.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 import pandas as pd
+import os
 
-file_path = 'gymworkout.csv'
-engine = create_engine("postgresql://postgres:12345678@localhost:5432/postgres")
-data = pd.read_csv(file_path)
-data.to_sql('Workout', engine, index=False, if_exists='append')
+csv_files = ['gymsession', 'gymworkout', 'mtworkout', 'runningworkout', 'otherworkout']
+for entry in range(len(csv_files)):
+    engine = create_engine("postgresql://postgres:12345678@localhost:5432/postgres")
+    file_path = f'{entry}.csv'
+    if os.path.exists(file_path):
+        data = pd.read_csv(file_path)
+        data.to_sql(entry, engine, index=False, if_exists='append')
